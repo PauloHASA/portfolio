ebaf57e20f59655d4e805d2b5feff55d70fd8ecb
Paulo Hasa <paulohasa@gmail.com>
2025-03-04 12:14:02 -0300

    Add react-router-dom and implement routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ParallaxProvider } from 'react-scroll-parallax';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ParallaxProvider>
-      <App />
+      <Router basename="/portfolio">
+        <App />
+      </Router>
     </ParallaxProvider>
   </StrictMode>
 );
